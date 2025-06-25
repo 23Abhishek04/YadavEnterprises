55e633d3d24c2b805723e35b1322076285064404
23Abhishek04 <abhishekbamane23@gmail.com>
2025-06-25 15:27:37 +0530

    Update: added new feature or fixed bug

--- src/app/ClientLayout.jsx
+++ src/app/ClientLayout.jsx
@@ -1,12 +1,22 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import Navbar from '@/components/Navbar';
+import Preloader from '@/components/PreLoader';
 
 export default function ClientLayout({ children }) {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return loading ?  <Preloader />: (
     <>
       <Navbar />
       {children}
     </>
   );
 }
+
